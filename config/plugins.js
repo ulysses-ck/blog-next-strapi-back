20741483da4593c81e3976e4b2dad995a7448dfa
ulysses-ck <ulisesapaza.280@gmail.com>
2024-04-24 08:22:55 -0300

    init project

---
+++ config/plugins.js
@@ -0,0 +1,16 @@
+module.exports = () => ({
+  graphql: {
+    enabled: true,
+    config: {
+      // set this to true if you want to enable the playground in production
+      playgroundAlways: false,
+    },
+  },
+  "apollo-sandbox": {
+    // enables the plugin only in development mode
+    // if you also want to use it in production, set this to true
+    // keep in mind that graphql playground has to be enabled
+    enabled: process.env.NODE_ENV === "production" ? false : true,
+    // endpoint: "https://tunneled-strapi.com/graphql", // OPTIONAL - endpoint has to be accessible from the browser
+  },
+});
